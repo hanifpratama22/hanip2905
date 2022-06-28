d5176b1e6745bb1fbcb6e41db86542d495a560fe
Mickyokta <micky.okta36@gmail.com>
2022-06-28 17:31:41 +0700

    .\index.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-// Insert your code here
+/* Algoritma
+ 1. Cari rumus keliling lingkaran
+ 2. Temukan panjang jari-jari lingkaran
+ 3. Kalikan dengan pi (3.14)
+ 4. Kalikan dengan 2
+ 5. Tampilkan hasil
+
+ Diketahui
+ jari-jari = 14
+ pi = 3.14
+
+ Ditanya
+ keliling lingkaran
+
+ Jawaban
+
 var pi = Math.PI;
 var jariJari;
+var keliling;
+
+STORE pi AS NUMBER WITH Math.PI
+STORE jariJari AS NUMBER WITH 14
+STORE keliling AS NUMBER WITH jariJari TIMES pi TIMES 2
+
+DISPLAY "Nilai pi: " CONCAT WITH pi
+DISPLAY "Nilai Jari-jari: " CONCAT WITH jariJari
+DISPLAY "Keliling lingkaran = " CONCAT WITH 'jari-jari x 2 x pi'
+DISPLAY "Keliling lingkaran = " CONCAT WITH jariJari CONCAT WITH 'x 2 x' CONCAT WITH pi
+DISPLAY "Keliling lingkaran = " keliling
+*/
